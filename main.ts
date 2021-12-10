fbc78a4f616b5f85fd1be752ba60b734e875c9cc
eloicanigueral <95910674+eloicanigueral@users.noreply.github.com>
2021-12-10 10:49:57 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    music.setVolume(255)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
 })
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("C5 C5 C5 B B B A A ", 120)
+    music.playMelody("A G G G F F F E ", 120)
+    music.playMelody("E E D D D C C C ", 120)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.stopMelody(MelodyStopOptions.All)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("C5 A B G A F G E ", 120)
+    basic.showLeds(`
+        # # # . #
+        . . # . #
+        # # . # #
+        # . # . .
+        # . # # #
+        `)
+})
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
